3fd3208ed4593f64d768e463fd0bcff774dc7c78
DanDCD <Daniel>
2021-11-26 00:59:14 +0000

    improvements

--- src/EntityCompSys/Components/Behaviours/Behaviour/Behaviour.gd
+++ src/EntityCompSys/Components/Behaviours/Behaviour/Behaviour.gd
@@ -2,3 +2,22 @@ extends Node
 class_name Behaviour
 
 
+# ABSTRACT METHOD - subclasses can use to create custom setup behavoir (called on ready after body implimented)
+func setUpBehaviour():
+	pass
+
+
+func cleanUpBehaviour():
+	pass
+
+
+
+
+func _ready():
+	
+
+
+
+func _notification(what):
+	if what == NOTIFICATION_PREDELETE:
+		cleanUpBehaviour()
--- src/EntityCompSys/Components/Behaviours/MovementBehaviours/MovementBehaviour/MovementBehaviour.gd
+++ src/EntityCompSys/Components/Behaviours/MovementBehaviours/MovementBehaviour/MovementBehaviour.gd
@@ -7,11 +7,6 @@ var speed: int = 0
 var velocity: Vector2 = Vector2.ZERO
 
 
-
-
-
-
-
 # sets the new body for the behaviour to focus - will return true if set successfully
 # fails if body not in movementbody group
 func setMovementBody(newBody: Object)->bool:
@@ -21,23 +16,51 @@ func setMovementBody(newBody: Object)->bool:
 	return true
 
 
+func preMoveBehaviour():
+	pass
+
+func postMoveBehaviour():
+	pass
+
+
+
+
+func findMovementBody():
+	var movementBodyArray:Array = get_parent().getComponentsByGroup("MovementBody")
+	if movementBodyArray.size() != 0 :
+		return movementBodyArray[0]
+
+
+
+# moves the movement body by a velocity
+# velocity will be calculated using direction and speed members
+# if a custom velocity is provided as an argument, this will be used instead
 func moveBody(custom_velocity = null):
+	preMoveBehaviour()
 	if movementBody == null:
 		return
 	if custom_velocity != null:
 		direction = direction.normalized()
 		velocity = direction * speed
 		movementBody.move(velocity)
+		postMoveBehaviour()
 	else:
 		movementBody.move(custom_velocity)
+		postMoveBehaviour()
 		
-func setUpMovementBody():
-	pass
+func setUp():
+	if movementBody == null:
+		setMovementBody(findMovementBody())
+	
 		
+		
+
+		
+
 		
 func _ready():
 	if get_parent().isSetUp():
-		setUpMovementBody()
+		setUp()
 	else:
-		get_parent().connect("EntitySetUp", self, "setUpMovementBody")
+		get_parent().connect("EntitySetUp", self, "setUp")
 		
